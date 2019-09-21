7e1a1c549e2f64d1416d855c191fc835da9940dd
storysteller <664374091@qq.com>
2019-09-21 22:30:12 +0800

    hw2

---
+++ SolarSystem/Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public Transform center;    
+    public float revolution;        
+    public float rotation;        
+    public float ry, rz;        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 axis = new Vector3(0, ry, rz);    
+        this.transform.RotateAround(center.position, axis, revolution * Time.deltaTime); 
+        this.transform.Rotate(Vector3.up * rotation * Time.deltaTime);    
+    } 
+
+}
