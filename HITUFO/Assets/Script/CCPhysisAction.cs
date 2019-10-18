b4ca99ab1c856d1f88c817cc1aef0b8660658a44
storysteller <664374091@qq.com>
2019-10-18 23:28:02 +0800

    hm5

---
+++ HITUFO/Assets/Script/CCPhysisAction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCPhysisAction : SSAction
+{
+    public float speedx;
+
+    public override void Start ()
+    {
+        if (!this.gameObject.GetComponent<Rigidbody>())
+        {
+            this.gameObject.AddComponent<Rigidbody>();
+        }
+        this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 9.8f * 0.6f, ForceMode.Acceleration);
+        this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(speedx, 0, 0), ForceMode.VelocityChange);
+    }
+
+    private CCPhysisAction()
+    {
+        
+    }
+    public static CCPhysisAction getAction(float speedx)
+    {
+        CCPhysisAction action = CreateInstance<CCPhysisAction>();
+        action.speedx = speedx;
+        return action;
+    }
+
+    override public void Update ()
+    {
+        if (transform.position.y <= 3)
+        {
+            Destroy(this.gameObject.GetComponent<Rigidbody>());
+            destroy = true;
+            CallBack.SSActionCallback(this);
+        }
+    }
+}
---
+++ HITUFO/Assets/Script/CCPhysisActionManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Interfaces;
+
+public class CCPhysisActionManager : SSActionManager, SSActionCallback, IActionManager
+{
+    int count = 0;//记录所有在移动的碟子的数量
+    public SSActionEventType Complete = SSActionEventType.Completed;
+
+    public void PlayDisk(Disk disk)
+    {
+        count++;
+        Complete = SSActionEventType.Started;
+        CCPhysisAction action = CCPhysisAction.getAction(disk.speed); //实例化为物理运动对象
+        addAction(disk.gameObject, action, this);
+    }
+
+    public void SSActionCallback(SSAction source) //运动事件结束后的回调函数
+    {
+        count--;
+        Complete = SSActionEventType.Completed;
+        source.gameObject.SetActive(false);
+    }
+
+    public bool IsAllFinished() //主要为了防止游戏结束时场景还有对象但是GUI按钮已经加载出来
+    {
+        if (count == 0) return true;
+        else return false;
+    }
+}
